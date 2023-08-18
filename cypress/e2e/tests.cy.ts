04709af59788e828407a88587b3e9da9fb07400d
Razvan Paraschivescu <razvan.paraschivescu@ubisoft.com>
2023-08-18 16:05:02 +0300

    removed repetitive step from tests

--- cypress/e2e/tests.cy.ts
+++ cypress/e2e/tests.cy.ts
@@ -3,19 +3,16 @@ import dragAndDrop from "../support/drag-and-drop";
 import { AppConfig } from "../framework/types/fixtures";
 
 const appConfigPath = "../fixtures/app-config.json";
-
 describe("seleniumbase.id demo tests", () => {
+  beforeEach(() => cy.fixture<AppConfig>(appConfigPath).then(cfg => cy.visit(cfg.url)))
+
   it("Text Input Field Accepts Input", () => {
     const text: string = "Hello World";
 
-    cy.fixture<AppConfig>(appConfigPath)
-      .then(cfg => {
-        cy.visit(cfg.url)
-          .get(Pom.TextInput)
-          .type(text)
-          .then(input => input.val().toString())
-          .then(value => expect(value).to.be.equal(text));
-      });
+    cy.get(Pom.TextInput)
+      .type(text)
+      .then(input => input.val().toString())
+      .then(value => expect(value).to.be.equal(text));
   });
 
 
@@ -23,54 +20,51 @@ describe("seleniumbase.id demo tests", () => {
     const text: string = "Set to 50%";
     const metterValueExpected: string = "0.5";
 
-    cy.fixture<AppConfig>(appConfigPath)
-      .then(cfg => {
-        cy.visit(cfg.url)
-          .get(Pom.SelectDropdown)
-          .select(text)
-          .then(select => select.val() as string)
-          .then(selected => expect(selected).to.be.equal("50%"))
-          .get(Pom.MetterBar)
-          .then(metterBar => metterBar.val().toString())
-          .then(metterVal => expect(metterVal.toString()).to.equal(metterValueExpected));
-      })
+    cy.get(Pom.SelectDropdown)
+      .select(1)
+      .then(select => select.val() as string)
+      .then(selected => expect(selected).to.be.equal("50%"))
+      .get(Pom.MetterBar)
+      .then(metterBar => metterBar.val().toString())
+      .then(metterVal => expect(metterVal).to.equal(metterValueExpected));
   });
 
   it("Button Color Changes On Click", () => {
     let initialExpected: string = "green";
     let afterExpected: string = "purple";
 
-    cy.fixture<AppConfig>(appConfigPath)
-      .then(cfg => {
-        cy.visit(cfg.url)
-          .get(Pom.Button)
-          .then(btn => btn.attr("style"))
-          .then(style => expect(style).to.include(initialExpected))
-          .get(Pom.Button)
-          .click()
-          .then(btn => btn.attr("style"))
-          .then(style => expect(style).to.include(afterExpected));
-      })
+    cy.get(Pom.Button)
+      .then(btn => btn.attr("style"))
+      .then(style => expect(style).to.include(initialExpected))
+      .get(Pom.Button)
+      .click()
+      .then(btn => btn.attr("style"))
+      .then(style => expect(style).to.include(afterExpected));
   });
 
   it("Drag And Drop Test", () => {
-    cy.fixture<AppConfig>(appConfigPath)
-      .then(cfg => {
-        cy.visit(cfg.url)
-          .get(Pom.Checkbox)
-          .click()
+
+    cy.get(Pom.Checkbox)
+      .click()
+      .get(Pom.Draggable)
+      .then(draggableInit => {
+        const {
+          left: initialX,
+          top: initialY
+        } = draggableInit.position();
+
+        cy.window()
+          .then(win => win.eval(dragAndDrop(Pom.Draggable, Pom.DragAndDropTo)))
           .get(Pom.Draggable)
-          .then(draggableInit => {
-            const { left: initialX, top: initialY } = draggableInit.position();
-            cy.window()
-              .then(win => win.eval(dragAndDrop(Pom.Draggable, Pom.DragAndDropTo)))
-              .get(Pom.Draggable)
-              .then(dragableAfter => {
-                const { left: afterX, top: afterY } = dragableAfter.position();
-                expect(afterX).to.be.greaterThan(initialX);
-                expect(afterY).to.be.equal(initialY);
-              });
-          })
+          .then(dragableAfter => {
+            const {
+              left: afterX,
+              top: afterY
+            } = dragableAfter.position();
+
+            expect(afterX).to.be.greaterThan(initialX);
+            expect(afterY).to.be.equal(initialY);
+          });
       });
-  })
-})
+  });
+});
